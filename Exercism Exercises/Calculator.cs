3422df104dc6450c8abbd1007526c6d3682703fe
Rachael Murtagh <rachaelmurtagh29@gmail.com>
2026-03-22 18:31:55 +0000

    Basics - Calculator Example

---
+++ Exercism Exercises/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercismCalculator
+{
+    internal class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
--- Exercism Exercises/Program.cs
+++ Exercism Exercises/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace HelloWorld
+﻿using ExercismCalculator;
+
+namespace ExercismMainProgram
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of class Calculator
+            var runCalculator = new Calculator();
+            // Add in the parameters
+            var sum_v1 = runCalculator.Add(1, 2);
+            // Invoking the method
+            Console.WriteLine(sum_v1);
         }
     }
 }
